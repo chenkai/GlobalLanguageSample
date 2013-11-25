f61441195ddc8e8b4133537540798e302852b1eb
chenkai <chenkaihome@live.cn>
2013-11-25 19:28:54 +0800

    fix some resource change

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -9,6 +9,8 @@ using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using GlobalLanguageDemo.Resources;
 
+using System.Resources;
+using System.Reflection;
 namespace GlobalLanguageDemo
 {
     public partial class MainPage : PhoneApplicationPage
@@ -20,12 +22,58 @@ namespace GlobalLanguageDemo
         }
 
         void MainPage_Loaded(object sender, RoutedEventArgs e)
-        {            
+        {     
+       
         }
 
         private void LanguageChanged_LP_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
         }
+
+
+        #region Swithc System Language When Application ReStart
+
+        #region Property Language
+        ResourceManager _englisthManager;
+        ResourceManager _chineseManager;
+
+        AppResources _englishResources;
+        AppResources _chineseResources;
+
+        LocalizedStrings locStrings;
+        #endregion
+
+        private void ChangeLanguageInit()
+        {
+            #region Switch UI English Language
+            _englisthManager = new System.Resources.ResourceManager("GlobalLanguageDemo.Resources.SwitchLanguage.AppResources-en-US", Assembly.Load("GlobalLanguageDemo"));
+            _englishResources = new AppResources();
+            #endregion
+
+            #region Switch UI Chinese Language
+            _chineseManager = new System.Resources.ResourceManager("GlobalLanguageDemo.Resources.SwitchLanguage.AppResources", Assembly.Load("GlobalLanguageDemo"));
+            locStrings = Application.Current.Resources["LocalizedStrings"] as LocalizedStrings;
+            _chineseResources = locStrings.LocalizedResources;
+            #endregion
+
+        }
+
+        private void SwitchLanguageByConfig(string saveLanguageConfig)
+        {
+            if (string.IsNullOrEmpty(saveLanguageConfig))
+                return;
+
+            switch (saveLanguageConfig)
+            {
+                case "Chinese Simplified":
+                    AppResources.ResourceManager = _chineseManager;
+                    break;
+                case "English":
+                    AppResources.ResourceManager = _englisthManager;
+                    break;       
+            }
+        }
+        #endregion
     }
 } 
