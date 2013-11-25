76849362cce351bc50be69fa6cfc6ff65ce4aceb
chenkai <chenkaihome@live.cn>
2013-11-25 16:17:50 +0800

    first commit empty project status

---
+++ LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using GlobalLanguageDemo.Resources;
+
+namespace GlobalLanguageDemo
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
